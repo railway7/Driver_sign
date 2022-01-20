0de64d71afe87d7fc41812f71a1f8826bcdb1598
spiritlhl <62994705+spiritLHL@users.noreply.github.com>
2022-01-20 22:23:08 +0800

    Update Sign_google.py

--- Sign_google.py
+++ Sign_google.py
@@ -41,7 +41,7 @@ def input_dependence():
     global driver, shadow
     # 启动浏览器内核
     opt = ChromeOptions()
-    opt.headless = True
+    opt.headless = False
     # path_e = os.getcwd() + r"\buster.crx"
     # opt.add_extension(path_e)
     # path_e = os.getcwd() + r"\AutoVerify.crx"
@@ -102,7 +102,7 @@ def main(url):
         list_site_urls = list_site_urls[0:14]
     print(5.6)
     if (len(list_urls) - len(list_site_urls)) > 1:
-        list_site_urls.append(list_site_urls[-1])
+        list_site_urls.append(url)
     list_urls.extend(list_site_urls)
     list_urls = random.sample(list_urls, len(list_urls))
     print(6)
