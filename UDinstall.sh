ed7f30940ca514852298859b391066a8a0127863
spiritlhl <62994705+spiritLHL@users.noreply.github.com>
2022-01-28 10:23:36 +0800

    Update UDinstall.sh

--- UDinstall.sh
+++ UDinstall.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 sudo -i
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get upgrade -y
-sudo apt-get update -y
-sudo apt-get -f install -y
-dpkg -i google-chrome-stable_current_amd64.deb -y
+sudo wget -N http://chromedriver.storage.googleapis.com/97.0.4692.20/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
+sudo chmod +x chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
 sudo apt install -f -y
-python3 -V
-apt --fix-broken install -y
+sudo python3 -V
+sudo apt --fix-broken install -y
 sudo apt-get install -y python3-pip -y
-wget https://npm.taobao.org/mirrors/chromedriver/97.0.4692.71/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-ln -sf /chromedriver /usr/bin/chromedriver
+sudo wget https://npm.taobao.org/mirrors/chromedriver/97.0.4692.71/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
+sudo chmod +x chromedriver
+sudo ln -sf /chromedriver /usr/bin/chromedriver
 sudo apt install chromium-driver -y
 sudo apt-get update -y
 sudo apt-get install xvfb -y
-wget https://raw.githubusercontent.com/spiritLHL/Gecko_sign/master/requirements.txt
-pip3 install -r requirements.txt
+sudo wget https://raw.githubusercontent.com/spiritLHL/Gecko_sign/master/requirements.txt
+sudo pip3 install -r requirements.txt
